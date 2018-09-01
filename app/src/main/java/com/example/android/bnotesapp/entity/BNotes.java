af4803bbce8d9656d5b9b548735c4cb3b22588e6
Baranidharan M <baranim18@gmail.com>
2018-09-01 21:03:01 +0530

    comments

--- app/src/main/java/com/example/android/bnotesapp/entity/BNotes.java
+++ app/src/main/java/com/example/android/bnotesapp/entity/BNotes.java
@@ -4,9 +4,6 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
 
-/**
- * Created by gowri on 25-08-2018.
- */
 
 @Entity
 public class BNotes {
