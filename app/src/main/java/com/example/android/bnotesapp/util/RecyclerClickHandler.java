561f06f243f39e4d1c43dbe7d4b10608b3aa4aa1
Baranidharan M <baranim18@gmail.com>
2018-09-01 21:04:14 +0530

    comments

--- app/src/main/java/com/example/android/bnotesapp/util/RecyclerClickHandler.java
+++ app/src/main/java/com/example/android/bnotesapp/util/RecyclerClickHandler.java
@@ -4,9 +4,6 @@ import android.view.View;
 
 import com.example.android.bnotesapp.entity.BNotes;
 
-/**
- * Created by gowri on 26-08-2018.
- */
 
 public interface RecyclerClickHandler {
     void OnClick(View view, BNotes bNotes);
