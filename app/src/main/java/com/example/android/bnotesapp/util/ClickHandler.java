682a44fb85f042515fd0d290460bc5cd6e5d5e83
Baranidharan M <baranim18@gmail.com>
2018-09-01 21:03:49 +0530

    comments

--- app/src/main/java/com/example/android/bnotesapp/util/ClickHandler.java
+++ app/src/main/java/com/example/android/bnotesapp/util/ClickHandler.java
@@ -2,10 +2,6 @@ package com.example.android.bnotesapp.util;
 
 import android.view.View;
 
-/**
- * Created by gowri on 27-08-2018.
- */
-
 public interface ClickHandler {
     void lockClick(View view);
 }
